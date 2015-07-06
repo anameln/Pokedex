8e7cc3ebcc8d7c62cd2648ce54e449febc42ad32
john&katrina <john_katrina@appacademy.io>
2015-07-06 13:49:44 -0400

    Phase 1A working

--- app/assets/javascripts/pokedex-1A.js
+++ app/assets/javascripts/pokedex-1A.js
@@ -1,5 +1,37 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
+  var $li = $("<li></li>");
+  var name = pokemon.get("name")
+  var poke = pokemon.get("poke_type")
+  $li.html("name: " + name + " poke_type: " + poke);
+
+  $li.addClass("poke-list-item");
+  this.$pokeList.append($li);
+
 };
+//
+// Pokedex.RootView.prototype.refreshPokemon = function () {
+//   debugger
+//   this.pokes.fetch({
+//     success: function({
+//       this.pokes.forEach(function (pmon) {
+//         console.log("hi");
+//         Pokedex.RootView.addPokemonToList(pmon);
+//       });
+//     });
+//   });
+//
+// };
+
+
 
 Pokedex.RootView.prototype.refreshPokemon = function () {
-};
+  // debugger
+  this.pokes.fetch({
+    success: function(){
+      this.pokes.forEach( function (pmon) {
+        Pokedex.rootView.addPokemonToList(pmon);
+      })
+    }.bind(this)
+  })
+
+}
